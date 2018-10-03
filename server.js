fe3e6517199f2a48e70da60833185d1404aa8ea6
Chace Powell <cpcpowell8@gmail.com>
2018-10-03 09:16:33 -0400

    begin server logic

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
 const express = require('express');
-const mysql = require('mysql');
+const hbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 8080;
+
+app.engine('.hbs', hbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+
+
+
+app.listen(PORT, () => console.log(`LISTENING ON PORT: ${PORT}`));
