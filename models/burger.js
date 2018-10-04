0fc31c9e5f52f00fb11ca361a5e47761f664cb4e
Chace Powell <cpcpowell8@gmail.com>
2018-10-04 09:50:25 -0400

    add all method to burger model

--- models/burger.js
+++ models/burger.js
@@ -1,10 +1,13 @@
 const orm = require('../config/orm');
 
 const burger = {
+    all: (cb) => {
+        orm.selectAll('burgers', res => cb(res));
+    },
     create: () => {
 
     },
     eat: () => {
-        
+
     }
 }
