08d6a7b69e48fc6ca11a85bb17496ad6a0d8f3cc
Chace Powell <cpcpowell8@gmail.com>
2018-10-05 17:57:00 -0400

    add logic for JAWSDB connection

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,16 @@
 const mysql = require('mysql');
+let connection;
 
-const connection = mysql.createConnection({
-    port: 3306,
-    user: 'root',
-    password: '',
-    database: 'burgers_db'
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        port: 3306,
+        user: 'root',
+        password: '',
+        database: 'burgers_db'
+    });
+}
 
 connection.connect(err => {
     if (err) console.log(err);
